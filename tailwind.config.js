363667af9fe527274a4fb2bb141bc40a0bd72cfe
Huong Nguyen <45929865+huongdevvn@users.noreply.github.com>
2022-09-19 15:01:54 +0700

    feat: improve some configs (#1)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+const tailwindcssRadix = require('tailwindcss-radix');
+
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [tailwindcssRadix()]
 };
